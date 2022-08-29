e590da3db9591ddce4ea1d40f7e7a93a787f0896
Erick Augusto <erickaugusto542@gmail.com>
2022-08-29 13:58:29 -0500

    first commit

---
+++ ai.py
@@ -0,0 +1,47 @@
+import json
+import random
+
+with open('conversations.json', 'r') as file:
+  conversations = json.loads(file.read().lower())
+
+'''
+Dice's coefficient implementation.
+'''
+def string_similarity(a, b):
+    """dice coefficient 2nt/(na + nb)."""
+    if not len(a) or not len(b): return 0.0
+    if len(a) == 1:  a=a+u'.'
+    if len(b) == 1:  b=b+u'.'
+    
+    a_bigram_list=[]
+    for i in range(len(a)-1):
+      a_bigram_list.append(a[i:i+2])
+    b_bigram_list=[]
+    for i in range(len(b)-1):
+      b_bigram_list.append(b[i:i+2])
+      
+    a_bigrams = set(a_bigram_list)
+    b_bigrams = set(b_bigram_list)
+    overlap = len(a_bigrams & b_bigrams)
+    dice_coeff = overlap * 2.0/(len(a_bigrams) + len(b_bigrams))
+    return dice_coeff
+
+# answers a question using artificial intelligence.
+def ai(question):
+  question = question.lower()
+  
+  possible_answers = []
+  for conversation in conversations:
+    if string_similarity(question, conversation[0]) > 0.5:
+      possible_answers.append([string_similarity(question, conversation[0])] + conversation[1:])
+  
+  if not possible_answers:
+    return None
+  
+  biggest = 0
+  for answer in possible_answers:
+    if answer[0] > biggest:
+      biggest = answer[0]
+      result = random.choice(answer[1:]) if answer[1:] else None
+  
+  return result
---
+++ bot.py
@@ -0,0 +1,58 @@
+'''
+A bot for asker.fun (https://asker.fun) website. Use the `Bot` class to easily control the bot.
+'''
+
+from time import sleep
+from random import choice
+from selenium import webdriver
+from ai import ai
+
+class Bot():
+  def __init__(self, sessionid):
+    self.sessionid = sessionid
+    self.webdriver = webdriver.Firefox()
+  
+  def login(self):
+    self.webdriver.get('https://asker.fun')
+    self.webdriver.add_cookie({"name": "sessionid", "value": self.sessionid})
+  
+  def get_bot_profile_url(self):
+    self.webdriver.get('https://asker.fun')
+    bot_profile_url = self.webdriver.find_element_by_id('pop-menu').find_elements_by_tag_name('a')[0].get_attribute('href')
+    return bot_profile_url
+  
+  def get_last_question_id(self):
+    '''
+    Get the last question id.
+    '''
+    self.webdriver.get('https://asker.fun/news')
+    while True:
+      try:
+        qid = self.webdriver.find_element_by_id('lista_de_questoes_recentes').find_elements_by_tag_name('li')[0].get_attribute('data-id')
+        break
+      except IndexError:
+        continue
+    return int(qid)
+  
+  def answer_question(self, qid):
+    self.webdriver.get('https://asker.fun/question/%d' % (qid))
+    try:
+      self.webdriver.find_element_by_id('sua-resposta').click()
+    except:
+      return
+    question = self.webdriver.find_elements_by_tag_name('h1')[0].text
+    answer = ai(question)
+    if not answer:
+      return
+    self.webdriver.find_element_by_id('sua-resposta').send_keys(answer)
+    self.webdriver.find_element_by_id('botao_enviar_resposta').click()
+  
+  def continually_answer_questions(self):
+    while True:
+      qid = self.get_last_question_id()
+      self.answer_question(qid)
+      sleep(choice((5, 10, 15, 20, 25, 30)))
+
+bot = Bot('p7hs4xekrcuwd84lcv32sah3t0uq1bnw')
+bot.login()
+bot.continually_answer_questions()
